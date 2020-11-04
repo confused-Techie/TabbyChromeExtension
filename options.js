66502b0dffaf6a44760d91cc08dd5c65cc7daf24
confused_techie <dev@lhbasics.com>
2020-11-03 17:47:58 -0800

    Added Server Save Notification

--- options.js
+++ options.js
@@ -6,6 +6,10 @@ saveServer.onclick = function() {
     console.log('Server saved as: '+document.getElementById("serverAddress").value);
     chrome.storage.sync.get(['server'], function(result) {
       console.log('Server settings currently: ' + result.server);
+      new Notification("Tabby", {
+        icon: "images/tabbyIcon48.png",
+        body: "Server Saved Successfully!"
+      });
     })
   });
 }
