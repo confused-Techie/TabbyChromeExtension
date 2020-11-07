ac0d681de4e7f42ef74b3f6ef6c7001adf9cc3f3
confused_techie <dev@lhbasics.com>
2020-11-07 15:29:26 -0800

    Badges, Notifications, More Settings Options
    Notifications on install or update to save ServerURL, Badges for Last Status Result, Setting for Status Timeout, Setting for Debug Mode.

--- background.js
+++ background.js
@@ -2,8 +2,11 @@
 
 
 chrome.runtime.onMessage.addListener( function(msg, sender, sendResponse) {
-//function tabbyBackground(msg) {
-  console.log("tabbyBackground");
+  chrome.storage.sync.get(['debug'], function(debug) {
+    if (debug.debug) {
+      console.log("Background Service Running...");
+    }
+  });
   if (msg.server == "reload") {
     reloadStatus(sendResponse);
   } else {
@@ -11,14 +14,22 @@ chrome.runtime.onMessage.addListener( function(msg, sender, sendResponse) {
   var xhr = new XMLHttpRequest();
   var userURI = msg.server;
   var apiRequestLoc = "/api?type=new&bm=";
-  console.log(userURI);
+  chrome.storage.sync.get(['debug'], function(debug) {
+    if (debug.debug) {
+      console.log("URI to send to server: "+userURI);
+    }
+  });
+
   chrome.storage.sync.get(['server'], function(resultServer) {
     xhr.open("GET", resultServer.server + apiRequestLoc + userURI, true);
     xhr.onreadystatechange = function() {
-      console.log("Ready state has changed. Likely meaning it sent");
       if (xhr.readyState == 4) {
         if (xhr.status == 200) {
-          console.log("StatusCode 200");
+          chrome.storage.sync.get(['debug'], function(debug) {
+            if (debug.debug) {
+              console.log("Request StatusCode is 200 OK");
+            }
+          });
           var resp = JSON.parse(xhr.responseText);
           //parse to see result
           if (resp == "Success") {
@@ -27,13 +38,21 @@ chrome.runtime.onMessage.addListener( function(msg, sender, sendResponse) {
             chrome.storage.sync.set({lastStatus: "Success", lastStatusTime: d.getTime()}, function() {
 
             });
-            console.log("Should save Success response...");
+            chrome.storage.sync.get(['debug'], function(debug) {
+              if (debug.debug) {
+                console.log("Should save Success response...");
+              }
+            });
+            chrome.browserAction.setBadgeBackgroundColor({color: "#00ba41"});
+            chrome.browserAction.setBadgeText({text: "OK"});
             sendResponse({response: "Success"});
           } else {
             var d = new Date();
             chrome.storage.sync.set({lastStatus: resp, lastStatusTime: d.getTime()}, function() {
 
             });
+            chrome.browserAction.setBadgeBackgroundColor({color: "#ba0000"});
+            chrome.browserAction.setBadgeText({text: "ERR"});
             sendResponse({response: resp});
 
           }
@@ -42,7 +61,8 @@ chrome.runtime.onMessage.addListener( function(msg, sender, sendResponse) {
           chrome.storage.sync.set({lastStatus: xhr.status, lastStatusTime: dd.getTime()}, function() {
 
           });
-
+          chrome.browserAction.setBadgeBackgroundColor({color: "#ba0000"});
+          chrome.browserAction.setBadgeText({text: "ERR"});
           sendResponse({response: xhr.status});
 
         }
@@ -56,27 +76,83 @@ return true;
 
 
 function reloadStatus(sendResponse) {
-  console.log("Checking if status should be reloaded...");
+  chrome.storage.sync.get(['debug'], function(debug) {
+    if (debug.debug) {
+      console.log("Checking if status should be reloaded...");
+    }
+  });
 
   chrome.storage.sync.get(['lastStatusTime'], function(statusTimeResult) {
     if (statusTimeResult.lastStatusTime != null) {
       var dd = new Date();
-      if (dd.getTime() > statusTimeResult.lastStatusTime + 15000) {
-        console.log("Status has expired...");
+      chrome.storage.sync.get(['statusTimeout'], function(statusTimeout) {
+        chrome.storage.sync.get(['debug'], function(debug) {
+          if (debug.debug) {
+            console.log("Status Timeout is: "+statusTimeout.statusTimeout);
+          }
+        });
+        var debugTimeoutExpiry = statusTimeResult.lastStatusTime+parseInt(statusTimeout.statusTimeout);
+        chrome.storage.sync.get(['debug'], function(debug) {
+          if (debug.debug) {
+            console.log("Current Time: " +dd.getTime()+" || Expiry: "+debugTimeoutExpiry);
+          }
+        });
+      if (dd.getTime() > statusTimeResult.lastStatusTime + parseInt(statusTimeout.statusTimeout)) {
+        chrome.storage.sync.get(['debug'], function(debug) {
+          if (debug.debug) {
+            console.log("Status has expired...");
+          }
+        });
+        chrome.browserAction.setBadgeText({text: ""});
         sendResponse({response: "Expired"});
       } else {
-        console.log("Status has not expired...");
+        chrome.storage.sync.get(['debug'], function(debug) {
+          if (debug.debug) {
+            console.log("Status has not expired...");
+          }
+        });
         chrome.storage.sync.get(['lastStatus'], function(statusResult) {
           if (statusResult.lastStatus == "Success") {
-            console.log("Status was Success");
+            chrome.storage.sync.get(['debug'], function(debug) {
+              if (debug.debug) {
+                console.log("Status was a Success");
+              }
+            });
             sendResponse({response: "Success"});
           } else {
-            console.log("Status was an ERROR");
+            chrome.storage.sync.get(['debug'], function(debug) {
+              if (debug.debug) {
+                console.log("Status was an ERROR");
+              }
+            });
             sendResponse({response: statusResult.lastStatus});
           }
         });
       }
+    });
     }
   });
   return true;
 }
+
+chrome.runtime.onInstalled.addListener(function(details) {
+  if (details.reason == "install") {
+    new Notification("Tabby Installation", {
+      icon: "images/tabbyIcon48.png",
+      body: "Make sure to save your Server URL, thanks for installing!"
+    });
+    //on first install set StatusTimeout to default value
+    chrome.storage.sync.set({statusTimeout: 15000});
+    //also set the debug to off
+    chrome.storage.sync.set({debug: false});
+  } else if (details.reason == "update") {
+    var thisVersion = chrome.runtime.getManifest().version;
+    new Notification("Tabby Updated", {
+      icon: "images/tabbyIcon48.png",
+      body: "Tabby updated from "+details.previousVersion+" to "+thisVersion+", make sure your Server URL is saved!"
+    });
+
+  }
+  //this will only fire when the extension is updated or first installed
+  //ignoring chrome_update or shared_module_update
+});
--- options.js
+++ options.js
@@ -1,22 +1,70 @@
 var saveServer = document.getElementById('serverAddressButton');
+var saveTimeout = document.getElementById('statusTimeoutButton');
+var debugCheckbox = document.getElementById('debuggingSlider');
 
 //should start onLoad, assigns the current value of the server to the Form, if one
 window.addEventListener('load', function load(event) {
   chrome.storage.sync.get(['server'], function(result) {
     document.getElementById("serverAddress").placeholder = result.server;
   });
+  chrome.storage.sync.get(['statusTimeout'], function(result) {
+    document.getElementById("statusTimeout").placeholder = result.statusTimeout;
+  });
+  chrome.storage.sync.get(['debug'], function(result) {
+    if (result.debug) {
+      var checkbox = document.getElementById('debuggingSlider');
+      checkbox.checked = true;
+    }
+    //if not checked leave at default which is unchecked
+  });
 });
 
 saveServer.onclick = function() {
   var serverLocation = document.getElementById("serverAddress").value;
   chrome.storage.sync.set({server: serverLocation}, function() {
-    console.log('Server saved as: '+document.getElementById("serverAddress").value);
+    chrome.storage.sync.get(['debug'], function(debug) {
+      if (debug.debug) {
+        console.log('Server saved as: '+document.getElementById("serverAddress").value);
+      }
+    });
     chrome.storage.sync.get(['server'], function(result) {
-      console.log('Server settings currently: ' + result.server);
+      chrome.storage.sync.get(['debug'], function(debug) {
+        if (debug.debug) {
+          console.log('Server settings currently: ' + result.server);
+        }
+      });
       new Notification("Tabby", {
         icon: "images/tabbyIcon48.png",
         body: "Server Saved Successfully!"
       });
-    })
+    });
   });
 }
+
+saveTimeout.onclick = function() {
+  var timeoutSetting = document.getElementById("statusTimeout").value;
+  chrome.storage.sync.set({statusTimeout: timeoutSetting}, function() {
+    chrome.storage.sync.get(['debug'], function(debug) {
+      if (debug.debug) {
+        console.log('Status Timeout Saved as: '+document.getElementById("statusTimeout").value);
+      }
+    });
+    new Notification("Tabby", {
+      icon: "images/tabbyIcon48.png",
+      body: "Status Timeout Saved Successfully!"
+    });
+  });
+}
+
+debugCheckbox.addEventListener( 'change', e => {
+  if (e.target.checked) {
+    chrome.storage.sync.set({debug: true}, function() {
+      console.log("Debugging Enabled...");
+    });
+    //e.target.checked = false; this can force the state
+  } else {
+    chrome.storage.sync.set({debug: false}, function() {
+      console.log("Debugging Disabled...");
+    });
+  }
+});
